afb14c85d59bcdfaa23baf0cf539a8b90230bc11
Rhoxio <rhoxiodbc@gmail.com>
2016-06-25 10:51:00 -0600

    cleaned up some nasty logging

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -19,28 +19,23 @@ class MatchController < ApplicationController
 			new_match.platform_id = response['platformId']
 
 			response['participants'].each do |participant|
-				p participant
 				spell_1 = participant['spell1Id']
 				spell_2 = participant['spell2Id']
 
-				p new_match.champions.build(champion_id: participant['championId'], 
-																	summoner_id: participant['summonerId'], 
-																	masteries: participant['masteries'], 
-																	runes: participant['runes'],
-																	summoner_spells: [spell_1, spell_2], 
-																	team: participant['teamId']
-																	)
+				new_match.champions.build(
+					champion_id: participant['championId'], 
+					summoner_id: participant['summonerId'], 
+					masteries: participant['masteries'], 
+					runes: participant['runes'],
+					summoner_spells: [spell_1, spell_2], 
+					team: participant['teamId']
+				)
 			end
-
-			p '-- New Match --'
-			p new_match
 
 			if new_match.save
 				p 'Match was saved!'
-				p match = Match.where(match_id: new_match.match_id.to_s)
 			else
 				p 'Match was not saved.'
-				p match = Match.where(match_id: new_match.match_id.to_s)
 			end
 
 		  respond_to do |format|
