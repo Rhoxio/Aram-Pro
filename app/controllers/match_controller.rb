7c017655a5fd2396561d91c31fd9211d15975f99
Rhoxio <rhoxiodbc@gmail.com>
2016-07-05 14:53:47 -0600

    checking if the utility to convert spacing worked

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -1,84 +1,84 @@
 class MatchController < ApplicationController
-	respond_to :json, :html
+  respond_to :json, :html
 
-	def get_one
-		if params[:id] && current_user
+  def get_one
+    if params[:id] && current_user
 
-			match = Match.find_by(match_id:params[:id])
+      match = Match.find_by(match_id:params[:id])
 
-			respond_to do |format|
-		    format.json{render :json => match, :include =>[:champions => {:include => :championbase}] }
-		  end	
+      respond_to do |format|
+        format.json{render :json => match, :include =>[:champions => {:include => :championbase}] }
+      end 
 
-		else
+    else
 
-			if !current_user
-				error = {error: "User not logged in."}
-			elsif !params[:id]
-				error = {error: "Param not specified"}
-			else
-				error = {error: "Error occured."}
-			end
+      if !current_user
+        error = {error: "User not logged in."}
+      elsif !params[:id]
+        error = {error: "Param not specified"}
+      else
+        error = {error: "Error occured."}
+      end
 
-			respond_to do |format|
-		    format.json{render :json => error }
-		  end	
+      respond_to do |format|
+        format.json{render :json => error }
+      end 
 
-		end
-	end
+    end
+  end
 
-	def create
-	end
+  def create
+  end
 
-	def current_match
+  def current_match
 
-		if params[:summoner_id]
+    if params[:summoner_id]
 
-			current_match = RiotAPI.get_current_match(params[:summoner_id])
+      current_match = RiotAPI.get_current_match(params[:summoner_id])
 
-			if current_match.instance_of?(Match)
-				new_match = Match.build_from_current_match(current_match, current_user)
+      if current_match.instance_of?(Match)
+        new_match = Match.build_from_current_match(current_match, current_user)
 
-			  respond_to do |format|
-			    format.json{render :json => new_match, :include =>[:champions => {:include => :championbase}] }
-			  end	
-			else
-				# current_match holds the HTTP error hash to be passed to the front.
-				respond_to do |format|
-			    format.json{render :json => current_match}
-			  end
-			end	
-		else
-		  respond_to do |format|
-		    format.json{render :json => {error: 'No summoner id.'} }
-		  end		
-		end
+        respond_to do |format|
+          format.json{render :json => new_match, :include =>[:champions => {:include => :championbase}] }
+        end 
+      else
+        # current_match holds the HTTP error hash to be passed to the front.
+        respond_to do |format|
+          format.json{render :json => current_match}
+        end
+      end 
+    else
+      respond_to do |format|
+        format.json{render :json => {error: 'No summoner id.'} }
+      end   
+    end
 
-	end
+  end
 
-	def recent_matches
-		if params[:summoner_id]
+  def recent_matches
+    if params[:summoner_id]
 
-				recent_arams = RiotAPI.get_recent_matches(params[:summoner_id])
+        recent_arams = RiotAPI.get_recent_matches(params[:summoner_id])
 
-				if recent_arams.is_a?(Array)
-					all_matches = Match.build_from_recent_matches(recent_arams, current_user)
+        if recent_arams.is_a?(Array)
+          all_matches = Match.build_from_recent_matches(recent_arams, current_user)
 
-					respond_to do |format|
-				    format.json{render :json => all_matches, :include =>[:champions => {:include => :championbase}]}
-				  end
-				else
-					# recent_arams holds the HTTP error hash to be passed to the front.
-					respond_to do |format|
-				    format.json{render :json => recent_arams}
-				  end
-				end
-			else
-			 	#There was some kind of error. 
-			 	respond_to do |format|
-			    format.json{render :json => {error: "No id param provided."}}
-			  end
-			end	
-	end
+          respond_to do |format|
+            format.json{render :json => all_matches, :include =>[:champions => {:include => :championbase}]}
+          end
+        else
+          # recent_arams holds the HTTP error hash to be passed to the front.
+          respond_to do |format|
+            format.json{render :json => recent_arams}
+          end
+        end
+      else
+        #There was some kind of error. 
+        respond_to do |format|
+          format.json{render :json => {error: "No id param provided."}}
+        end
+      end 
+  end
 
 end
